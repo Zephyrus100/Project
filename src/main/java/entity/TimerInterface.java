16fd86ef2d3266a591ed1f41d6cb9993fd490044
Ying XU <yingxu@Yings-MacBook-Pro.local>
2024-11-08 14:09:59 -0500

    the local timer entity class

---
+++ src/main/java/entity/TimerInterface.java
@@ -0,0 +1,23 @@
+package entity;
+
+/**
+ * The simple representation of timer in our program.
+ */
+public interface Timer {
+
+    /**
+     * Start the timer.
+     */
+    void start();
+
+    /**
+     * Pause the timer.
+     */
+    void pause();
+
+    
+
+
+
+
+}
