cc396a5e171a9e713914a06e32c1111c3ba149a2
mattm <mattmike2004@gmail.com>
2024-12-02 17:20:30 -0500

    Created Unit Tests

--- src/test/java/use_case/local_timer/LocalTimerInteractorTest.java
+++ src/test/java/use_case/local_timer/LocalTimerInteractorTest.java
@@ -2,11 +2,8 @@ package use_case.local_timer;
 import static org.junit.jupiter.api.Assertions.*;
 
 import data_access.InMemoryLocalTimerDataAccess;
+import entity.*;
 import org.junit.jupiter.api.Test;
-import entity.LocalTimer;
-import entity.LocalTimerFactory;
-import entity.User;
-import entity.UserFactory;
 import data_access.InMemoryUserDataAccessObject;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -15,27 +12,63 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class LocalTimerInteractorTest {
+class LocalTimerInteractorTest {
 
     @Test
     void successTest() {
         LocalTimerInputData inputData = new LocalTimerInputData("start");
         LocalTimerDataAccessInterface timerRepo = new InMemoryLocalTimerDataAccess();
+
         LocalTimerOutputBoundary successPresenter = new LocalTimerOutputBoundary() {
 
             @Override
             public void prepareSuccessView(LocalTimerOutputData timerOutputData) {
-                assertEquals("Timer started", timerOutputData.getTimerState());
+                assertEquals("Running", timerOutputData.getTimerState());
                 assertTrue(timerRepo.existsTimer());
-
             }
 
             @Override
             public void prepareFailView(String error) {
                 fail("Use case failure is unexpected.");
             }
-            // TODO: Maybe more test in the future but not sure
-            // TODO: Still need to test the time duration.
         };
+        LocalTimerInteractor interactor = new LocalTimerInteractor(successPresenter, new LocalTimerFactory(), timerRepo);
+        interactor.execute(inputData);
+    }
+
+    @Test
+    void pauseFailTest() {
+        LocalTimerInputData inputData = new LocalTimerInputData("pause");
+        LocalTimerDataAccessInterface timerRepo = new InMemoryLocalTimerDataAccess();
+        LocalTimerFactory timerFactory = new LocalTimerFactory();
+        LocalTimerOutputBoundary successPresenter = new LocalTimerOutputBoundary() {
+            public void prepareSuccessView(LocalTimerOutputData timerOutputData) {
+                fail("Use case success is unexpected.");
+            }
+            public void prepareFailView(String error) {
+                assertEquals("Timer is not running", error);
+            }
+        };
+        LocalTimerInteractor interactor = new LocalTimerInteractor(successPresenter, timerFactory, timerRepo);
+        interactor.execute(inputData);
+    }
+
+    @Test
+    void alreadyRunningTest() {
+        LocalTimerInputData inputData = new LocalTimerInputData("pause");
+        LocalTimerDataAccessInterface timerRepo = new InMemoryLocalTimerDataAccess();
+        LocalTimerFactory timerFactory = new LocalTimerFactory();
+
+
+        LocalTimerOutputBoundary successPresenter = new LocalTimerOutputBoundary() {
+            public void prepareSuccessView(LocalTimerOutputData timerOutputData) {
+                fail("Use case success is unexpected.");
+            }
+            public void prepareFailView(String error) {
+                assertEquals("Timer is not running", error);
+            }
+        };
+        LocalTimerInteractor interactor = new LocalTimerInteractor(successPresenter, timerFactory, timerRepo);
+        interactor.execute(inputData);
     }
 }
